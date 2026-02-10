65cd74c85f0222b353ada5bdfa30a4acfaeed287
fadlur <fadlur612@gmail.com>
2026-02-10 20:11:11 +0700

    fix(config): ensure correct basePath and assetPrefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const isProd = process.env.NODE_ENV === "production";
+const repoName = "portfolio";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: isProd ? "/portfolio" : "",
-	output: "export",
-	images: {
-		unoptimized: true,
-	},
+  output: "export",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
