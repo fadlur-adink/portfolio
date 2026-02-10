f4aeb648a4015d2a4ceb293ddbdcc8d27811bc6d
fadlur <fadlur612@gmail.com>
2026-02-10 19:22:44 +0700

    chore(config): disable image optimization for static export compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
