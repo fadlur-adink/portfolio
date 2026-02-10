f98122f183b177c16e62666d74d8e0c512686d4e
fadlur <fadlur612@gmail.com>
2026-02-10 20:01:13 +0700

    feat: Add conditional basePath for production builds and define `isProd` variable.

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+	basePath: isProd ? "/portfolio" : "",
+	output: "export",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default withNextIntl(nextConfig);
